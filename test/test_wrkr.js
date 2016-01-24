c36680530e38370bb5371c1560b9d128b879bc56
Paul Wijhenke <paul@paulsplace.nl>
2016-01-24 16:41:35 +0100

    Cleanup tests  & included some documentation

--- test/test_wrkr.js
+++ test/test_wrkr.js
@@ -6,39 +6,56 @@ var debug = require('debug')('wrkr:tests');
 var expect = require('expect.js');
 var randomstring = require('random-string');
 
+// Overwrite process.env for the test database name
+process.env.NODE_ENV = 'unittest';
+
 var WrkrMongodb = require('../lib/wrkr_mongodb');
 var Wrkr = require('../lib/wrkr');
 
 
-// Globals
-//
-var testEventName = 'event_'+randomstring();
-var testQueueName = 'queue_'+randomstring();
-var testTid       = 'tid_'+randomstring();
-
-
-// We use this emitter to deliver our events back to the tests
-var testEmitter = new EventEmitter();
-
 // Initialize our workerInterface
 var wrkr = new Wrkr({
-  store: new WrkrMongodb()
+  backend: new WrkrMongodb({
+    host:              'localhost',
+    port:              27017,
+    name:              'wrkr' + (process.env.NODE_ENV ? '_'+process.env.NODE_ENV : ''),
+    user:              '',
+    pswd:              '',
+    dbOpt:             {w: 1},  // Write concern
+
+    pollInterval:      50,      // default: 500, regular polling time (waiting for new items)
+    pollIntervalBusy:  5,       // default: 20 next-item-polling-interval after processing an item
+
+    errorRetryTime:    500,     // default: 5000, on error retry timer
+                                //    (not so happy with auto-retrying though)
+  })
 });
 
 
-// Mocha's Before and After
-//
-before( function (done) {
-  wrkr.start(done);
-});
-after( function (done) {
-  wrkr.stop(done);
-});
+// Start testing
+describe('backend - start', testStart);
+describe('basic operations', testBasic);
+describe('backend - stop', testStop);
 
 
-// Setup listener
-//
-describe('basic operations', function () {
+
+function testStart() {
+  it('starts backend', function (done) {
+    wrkr.start(done);
+  });
+}
+
+function testStop() {
+  it('stops backend', function (done) {
+    wrkr.stop(done);
+  });
+}
+
+function testBasic() {
+  var testEmitter = new EventEmitter();
+  var testTid = randomstring();
+  var testEventName = 'event_'+testTid;
+  var testQueueName = 'queue_'+testTid;
 
   // Test function to emit received Wrkr messages (back to our tests)
   function emitEvent(event, done) {
@@ -68,12 +85,11 @@ describe('basic operations', function () {
   it('receives our event', function (done) {
     testEmitter.on(testEventName, function (event) {
       expect(event.id).not.to.be(undefined);
-      expect(event.created).not.to.be(undefined);
+      expect(event.created).not.to.be.within(new Date(), new Date(Date.now() - 1000));
       expect(event.name).to.be(testEventName);
       expect(event.queue).to.be(testQueueName);
       expect(event.tid).to.be(testTid);
-
       return done(null);
     });
   });
-});
+}
